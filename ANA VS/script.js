830586ddeff2d9cbb563b8cd8e0082c92254514c
anaschrx <114492699+anaschrx@users.noreply.github.com>
2024-08-16 16:44:40 -0300

    Add files via upload

---
+++ ANA VS/script.js
@@ -0,0 +1,94 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+      enunciado:
+        "Qual é a principal característica do boto cor de rosa de acordo com o folclore brasileiro?",
+      alternativas: [
+        {
+          texto: "Ele é conhecido por transformar-se em um homem encantador para seduzir mulheres.",
+          afirmacao: "afirmação",
+        },
+        {
+          texto: "Ele é famoso por ser um protetor das crianças que brincam perto da água.",
+          afirmacao: "afirmação",
+        },
+      ],
+    },
+    {
+      enunciado:
+        "Qual é a principal consequência de um encontro com o boto cor de rosa, segundo a tradição?",
+      alternativas: [
+        {
+          texto:
+            "A mulher que encontra o boto pode engravidar e ter um filho com ele.",
+          afirmacao: "afirmação",
+        },
+        {
+          texto:
+            "A pessoa que encontra o boto ganha um desejo concedido.",
+          afirmacao: "afirmação",
+        },
+      ],
+    },
+    {
+      enunciado:
+        "Qual é a origem das lendas sobre o boto cor de rosa na cultura amazônica?",
+      alternativas: [
+        {
+          texto:
+            "Elas surgiram como uma explicação para desaparecimentos e eventos misteriosos na comunidade ribeirinha.",
+          afirmacao: "afirmação",
+        },
+        {
+          texto:
+            "Elas foram criadas para ensinar crianças sobre os perigos de nadar em águas profundas.",
+          afirmacao: "afirmação",
+        },
+      ],
+    },
+  ];
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal= "";
+
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+     caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
+
+
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa))
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSeLecionada){
+    const afirmacoes = opcaoSeLecionada.afirmacoes;
+    historiaFinal += afirmacoes + " ";
+        atual++;
+        mostraPergunta();
+}
+function mostraResultado() {
+    caixaPerguntas.textContent = "No folclore, o boto cor de rosa é frequentemente descrito como um ser que se transforma em um homem atraente para seduzir mulheres, especialmenteem festas à noite. Segundo a lenda, uma mulher que tem um encontro com o boto pode engravidar e, às vezes, o boto desaparece depois do evento, deixando a mulhercom um filho. As lendas do boto muitas vezes surgem como explicações para eventos misteriosos ou desaparecimentos inexplicáveis nas comunidades ribeirinhas da Amazônia.";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
+    mostraPergunta();
+
